6923ebdcf8287ecbc169389e51284ef0eb529ab5
zhouhongfa <zhouhongfa@gz-yibo.com>
2019-06-15 14:06:33 +0800

    update: 插件页增删除查改功能基本实现；后面再进行优化

--- src/api/datax-plugin.js
+++ src/api/datax-plugin.js
@@ -9,3 +9,34 @@ export function getList(params) {
     params
   })
 }
+
+export function fetchPlugin(params) {
+  return request({
+    url: '/dataxPlugin/' + params,
+    method: 'get'
+  })
+}
+
+export function updatePlugin(data) {
+  return request({
+    url: '/dataxPlugin/',
+    method: 'put',
+    data
+  })
+}
+
+export function createPlugin(data) {
+  return request({
+    url: '/dataxPlugin/',
+    method: 'post',
+    data
+  })
+}
+
+export function deletePlugin(data) {
+  return request({
+    url: '/dataxPlugin/',
+    method: 'delete',
+    params: data
+  })
+}
