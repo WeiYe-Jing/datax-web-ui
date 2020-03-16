e8cd63a550badf2fc3953097625134ee0f1d32f0
water <672684719@qq.com>
2020-03-16 17:08:07 +0800

    #3

--- src/views/datax/jdbc-datasource/index.vue
+++ src/views/datax/jdbc-datasource/index.vue
@@ -3,10 +3,10 @@
     <div class="filter-container">
       <el-input v-model="listQuery.datasourceName" clearable placeholder="数据源名称" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
       <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="fetchData">
-        Search
+        搜索
       </el-button>
       <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
-        Add
+        添加
       </el-button>
       <!-- <el-checkbox v-model="showReviewer" class="filter-item" style="margin-left:15px;" @change="tableKey=tableKey+1">
         reviewer
@@ -42,16 +42,16 @@
       <el-table-column label="jdbc驱动类" width="200" align="center" :show-overflow-tooltip="true">
         <template slot-scope="scope">{{ scope.row.jdbcDriverClass }}</template>
       </el-table-column>
-      <el-table-column label="comments" width="150" align="center">
+      <el-table-column label="注释" width="150" align="center">
         <template slot-scope="scope">{{ scope.row.comments }}</template>
       </el-table-column>
       <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
         <template slot-scope="{row}">
           <el-button type="primary" size="mini" @click="handleUpdate(row)">
-            Edit
+            编辑
           </el-button>
           <el-button v-if="row.status!='deleted'" size="mini" type="danger" @click="handleDelete(row)">
-            Delete
+            删除
           </el-button>
         </template>
       </el-table-column>
@@ -96,13 +96,13 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">
-          Cancel
+          取消
         </el-button>
         <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">
-          Confirm
+          确认
         </el-button>
         <el-button type="primary" @click="testDataSource()">
-          Test
+          测试连接
         </el-button>
       </div>
     </el-dialog>
